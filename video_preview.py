c533debb22ac706b03d83d990d8b32c2b5ef47de
Alistair Walsh <alistair.walsh@me.com>
2015-05-31 15:01:33 +1000

    changed preview time video_preview.py

--- video_preview.py
+++ video_preview.py
@@ -4,8 +4,9 @@ import picamera
 from time import sleep
 
 with picamera.PiCamera() as camera:
-    camera.resolution = (1280, 720)
+    camera.resolution = (640, 480)
     camera.framerate = 24
     camera.start_preview()
-    sleep(2)
+    sleep(10)
+    camera.stop_preview()
     
