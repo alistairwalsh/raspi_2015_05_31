fde16bec38db36f0d58dfa255b1591d73d4aa074
Alistair Walsh <alistair.walsh@me.com>
2015-05-31 14:50:01 +1000

    first of video_preview.py

---
+++ video_preview.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+import picamera
+from PIL import Image
+from time import sleep
+
+with picamera.PiCamera() as camera:
+    camera.resolution = (1280, 720)
+    camera.framerate = 24
+    camera.start_preview()
+    time.sleep(2)
+    
